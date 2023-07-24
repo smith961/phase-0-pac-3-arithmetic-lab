f1a325c0e7e66ab320393a959d4151a99fd141e9
unknown <smithbabawande@gmail.com>
2023-07-24 17:05:39 +0100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(number1, number2){
+   let  number = number1 + number2;
+    return number
+}
+
+function subtract(number1, number2){
+   let number = number1 - number2;
+   return number;
+}
+
+function multiply (number1, number2){
+   let number =  number1 * number2;
+   return number;
+}
+
+function divide(number1, number2){
+    let number = number1 / number2;
+    return number
+}
+let n = 0;
+
+function increment(n) {
+    return n +=1;
+}
+
+function decrement(n) {
+   return n-= 1;
+}
+function makeInt(n) {
+   return n = parseInt(n,10);
+   
+}
+
+function preserveDecimal(n){
+    return n= parseFloat(n);
+}
